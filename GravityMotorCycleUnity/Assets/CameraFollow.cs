c48c9d9f7a55ed3d7b1f0f7c99a29887248fc128
HaydenLiu <huangxunliu@gmail.com>
2020-07-25 09:08:04 +1000

    gameadd

---
+++ GravityMotorCycleUnity/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    Vector3 camera;
+    void Start()
+    {
+        camera = target.position;
+        camera.z = -10;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera = target.position;
+        camera.z = -10;
+        transform.position = camera;
+    }
+}
---
+++ GravityMotorCycleUnity/Assets/motorControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motorControl : MonoBehaviour
+{
+    public WheelJoint2D front;
+    public WheelJoint2D back;
+   [SerializeField] float speed = 1500f;
+    [SerializeField] float rotate = 1500f;
+    JointMotor2D motor;
+    Rigidbody2D rg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor.maxMotorTorque = 10000;
+        rg = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        motor.motorSpeed = -speed * Input.GetAxis("Vertical");
+        rg.AddTorque(rotate * Input.GetAxis("Horizontal")*Time.deltaTime);
+
+        front.motor = motor;
+        back.motor = motor;
+    }
+
+}
